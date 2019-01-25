95fe6ee46bb1ed09965c6210cb216f50093341cd
Magrit <mikael.pettersson201@gmail.com>
2019-01-25 18:31:32 +0100

    rustfmt everything

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-mod renderer;
 mod math;
-use renderer::*;
+mod renderer;
 use glium::index::PrimitiveType;
-use glium::{glutin, Surface, uniform, implement_vertex};
+use glium::{glutin, implement_vertex, uniform, Surface};
+use renderer::*;
 use std::env;
 
 use notify::{RecommendedWatcher, RecursiveMode, Watcher};
@@ -11,153 +11,161 @@ use std::thread;
 use std::time::{Duration, SystemTime};
 
 fn watch(path: std::path::PathBuf, sender: std::sync::mpsc::Sender<notify::DebouncedEvent>) {
-  let (tx, rx) = channel();
+    let (tx, rx) = channel();
 
-  let mut shader_watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_millis(50)).unwrap();
-  loop {
-    shader_watcher
-      .watch(path.clone(), RecursiveMode::Recursive)
-      .unwrap();
-    let event = rx.recv().unwrap();
-    sender.send(event).unwrap();
-  }
+    let mut shader_watcher: RecommendedWatcher =
+        Watcher::new(tx, Duration::from_millis(50)).unwrap();
+    loop {
+        shader_watcher
+            .watch(path.clone(), RecursiveMode::Recursive)
+            .unwrap();
+        let event = rx.recv().unwrap();
+        sender.send(event).unwrap();
+    }
 }
 
 fn main() {
-  let some_vec = math::Vec3::new(0.0, 1.0, 2.0);
-  let some_other_vec = math::Vec3::new(0.0, 1.0, 2.0);
-  let some_third_vec = some_vec.add(some_other_vec);
+    let some_vec = math::Vec3::new(0.0, 1.0, 2.0);
+    let some_other_vec = math::Vec3::new(0.0, 1.0, 2.0);
+    let some_third_vec = some_vec.add(some_other_vec);
 
-  let some_vec4 = math::Vec4::from_xyz(0.0, 1.0, 5.0);
-  let some_quat = math::quat_new();
+    let some_vec4 = math::Vec4::from_xyz(0.0, 1.0, 5.0);
+    let some_quat = math::quat_new();
 
+    let some_mat4 = math::Mat4::from_translation([10.0, 5.0, 5.0]);
+    let some_other_mat4 = math::Mat4::from_translation([10.0, 0.0, 5.0]);
+    let third_mat4 = some_mat4.mul(&some_other_mat4);
 
-  let some_mat4 = math::Mat4::from_translation([10.0, 5.0, 5.0]);
-  let some_other_mat4 = math::Mat4::from_translation([10.0, 0.0, 5.0]);
-  let third_mat4 = some_mat4.mul(&some_other_mat4);
+    println!("{:?}", some_third_vec);
+    println!("{:?}", third_mat4);
+    let multiplied_vec = math::mat4_mul_vec3(&third_mat4, &some_third_vec);
+    let multiplied_vec4 = math::mat4_mul_vec4(&third_mat4, &some_vec4);
+    println!("{:?}", multiplied_vec);
+    println!("{:?}", multiplied_vec4);
+    let mut events_loop = glutin::EventsLoop::new();
+    let window = glutin::WindowBuilder::new()
+        .with_title("mikpe_demo")
+        .with_dimensions(glutin::dpi::LogicalSize::new(1024.0, 768.0));
+    let context = glutin::ContextBuilder::new().with_vsync(true);
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
 
-  println!("{:?}", some_third_vec );
-  println!("{:?}", third_mat4 );
-  let multiplied_vec = math::mat4_mul_vec3(&third_mat4, &some_third_vec);
-  let multiplied_vec4 = math::mat4_mul_vec4(&third_mat4, &some_vec4);
-  println!("{:?}", multiplied_vec );
-  println!("{:?}", multiplied_vec4 );
-  let mut events_loop = glutin::EventsLoop::new();
-  let window = glutin::WindowBuilder::new()
-    .with_title("mikpe_demo")
-    .with_dimensions(glutin::dpi::LogicalSize::new(1024.0, 768.0));
-  let context = glutin::ContextBuilder::new().with_vsync(true);
-  let display = glium::Display::new(window, context, &events_loop).unwrap();
+    let mut running = true;
+    let (tx, rx) = channel();
+    let mut current_path = env::current_exe().unwrap();
 
-  let mut running = true;
-  let (tx, rx) = channel();
-  let mut current_path = env::current_exe().unwrap();
-
-  current_path.pop();
-  let path = current_path.join("../../assets/shaders");
-  let mut shader_list: Vec<shading::Shader> = Vec::new();
-  if path.is_dir() {
-    for entry in path.read_dir().expect("Read path failed!") {
-      if let Ok(entry) = entry {
-        let some_shader = shading::Shader::read(entry.path().clone());
-        shader_list.push(some_shader);
-      }
-    }
-  }
-
-  let child = thread::spawn(move || {
-    watch(path, tx.clone());
-  });
-
-  //TODO: make a pass out of this:
-  let vertex_buffer = {
-    #[derive(Copy, Clone)]
-    struct Vertex {
-      position: [f32; 2],
-    }
-    glium::implement_vertex!(Vertex, position);
-    glium::VertexBuffer::new(
-      &display,
-      &[
-        Vertex {
-          position: [0.0, 0.0],
-        },
-        Vertex {
-          position: [0.0, 0.0],
-        },
-        Vertex {
-          position: [0.0, 0.0],
-        },
-      ],
-    ).unwrap()
-  };
-
-  let index_buffer =
-    glium::IndexBuffer::new(&display, PrimitiveType::TriangleStrip, &[0 as u16, 1, 2]).unwrap();
-
-  let mut vert_shader = String::new();
-  let mut frag_shader = String::new();
-  for shader in shader_list {
-    match shader.shadertype {
-      shading::ShaderType::Fragment => frag_shader = shader.code.clone(),
-      shading::ShaderType::Vertex => vert_shader = shader.code.clone(),
-    }
-  }
-  let mut program = glium::Program::from_source(&display, &vert_shader, &frag_shader, None).unwrap();
-  //TODO: structure code in a more sensical manner
-  //TODO: Create better renderer-functions to wrap glutin
-  //TODO: Create some material-structuringish
-  
-
-  while running {
-    
-    let begin = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().subsec_millis() as f32;
-    let mut target = display.draw();
-    target
-      .draw(
-        &vertex_buffer,
-        &index_buffer,
-        &program,
-        &glium::uniform!{
-          time: begin,
-        },
-        &Default::default(),
-      ).unwrap();
-    target.finish().unwrap();
-
-    match rx.try_recv() {
-      Ok(event) => match event {
-        notify::DebouncedEvent::NoticeWrite(path) => println!("NoticeWrite to path: {:?}", path),
-        notify::DebouncedEvent::Write(path) => {
-          println!("Wrote to path: {:?}", path);
-          let some_shader = renderer::shading::Shader::read(path);
-          match some_shader.shadertype {
-            shading::ShaderType::Fragment => frag_shader = some_shader.code.clone(),
-            shading::ShaderType::Vertex => vert_shader = some_shader.code.clone(),
-          }
-          program = glium::Program::from_source(&display, &vert_shader, &frag_shader, None).unwrap();
-        },
-        notify::DebouncedEvent::Remove(path) => println!("Removed path: {:?}", path),
-        notify::DebouncedEvent::Create(path) => println!("Created to path: {:?}", path),
-        _ => (),
-      },
-      _ => (),
-    }
-
-    events_loop.poll_events(|event| match event {
-      glutin::Event::WindowEvent { event, .. } => match event {
-        glutin::WindowEvent::Touch(touch) => println!("FOOBAR {:?}", touch),
-        glutin::WindowEvent::CloseRequested => running = false,
-        glutin::WindowEvent::Resized(logical_size) => {
-          println!(
-            "Got some resize event w{}:h{}",
-            logical_size.width, logical_size.height
-          );
+    current_path.pop();
+    let path = current_path.join("../../assets/shaders");
+    let mut shader_list: Vec<shading::Shader> = Vec::new();
+    if path.is_dir() {
+        for entry in path.read_dir().expect("Read path failed!") {
+            if let Ok(entry) = entry {
+                let some_shader = shading::Shader::read(entry.path().clone());
+                shader_list.push(some_shader);
+            }
         }
-        _ => (),
-      },
-      _ => (),
-    })
-  }
-  child.join().unwrap();
+    }
+
+    let child = thread::spawn(move || {
+        watch(path, tx.clone());
+    });
+
+    //TODO: make a pass out of this:
+    let vertex_buffer = {
+        #[derive(Copy, Clone)]
+        struct Vertex {
+            position: [f32; 2],
+        }
+        glium::implement_vertex!(Vertex, position);
+        glium::VertexBuffer::new(
+            &display,
+            &[
+                Vertex {
+                    position: [0.0, 0.0],
+                },
+                Vertex {
+                    position: [0.0, 0.0],
+                },
+                Vertex {
+                    position: [0.0, 0.0],
+                },
+            ],
+        )
+        .unwrap()
+    };
+
+    let index_buffer =
+        glium::IndexBuffer::new(&display, PrimitiveType::TriangleStrip, &[0 as u16, 1, 2]).unwrap();
+
+    let mut vert_shader = String::new();
+    let mut frag_shader = String::new();
+    for shader in shader_list {
+        match shader.shadertype {
+            shading::ShaderType::Fragment => frag_shader = shader.code.clone(),
+            shading::ShaderType::Vertex => vert_shader = shader.code.clone(),
+        }
+    }
+    let mut program =
+        glium::Program::from_source(&display, &vert_shader, &frag_shader, None).unwrap();
+    //TODO: structure code in a more sensical manner
+    //TODO: Create better renderer-functions to wrap glutin
+    //TODO: Create some material-structuringish
+
+    while running {
+        let begin = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .subsec_millis() as f32;
+        let mut target = display.draw();
+        target
+            .draw(
+                &vertex_buffer,
+                &index_buffer,
+                &program,
+                &glium::uniform! {
+                  time: begin,
+                },
+                &Default::default(),
+            )
+            .unwrap();
+        target.finish().unwrap();
+
+        match rx.try_recv() {
+            Ok(event) => match event {
+                notify::DebouncedEvent::NoticeWrite(path) => {
+                    println!("NoticeWrite to path: {:?}", path)
+                }
+                notify::DebouncedEvent::Write(path) => {
+                    println!("Wrote to path: {:?}", path);
+                    let some_shader = renderer::shading::Shader::read(path);
+                    match some_shader.shadertype {
+                        shading::ShaderType::Fragment => frag_shader = some_shader.code.clone(),
+                        shading::ShaderType::Vertex => vert_shader = some_shader.code.clone(),
+                    }
+                    program =
+                        glium::Program::from_source(&display, &vert_shader, &frag_shader, None)
+                            .unwrap();
+                }
+                notify::DebouncedEvent::Remove(path) => println!("Removed path: {:?}", path),
+                notify::DebouncedEvent::Create(path) => println!("Created to path: {:?}", path),
+                _ => (),
+            },
+            _ => (),
+        }
+
+        events_loop.poll_events(|event| match event {
+            glutin::Event::WindowEvent { event, .. } => match event {
+                glutin::WindowEvent::Touch(touch) => println!("FOOBAR {:?}", touch),
+                glutin::WindowEvent::CloseRequested => running = false,
+                glutin::WindowEvent::Resized(logical_size) => {
+                    println!(
+                        "Got some resize event w{}:h{}",
+                        logical_size.width, logical_size.height
+                    );
+                }
+                _ => (),
+            },
+            _ => (),
+        })
+    }
+    child.join().unwrap();
 }
--- src/math.rs
+++ src/math.rs
@@ -1,55 +1,44 @@
+mod mat4;
 mod vec3;
 mod vec4;
-mod mat4;
 
-
+pub use self::mat4::Mat4;
 pub use self::vec3::Vec3;
 pub use self::vec4::Vec4;
-pub use self::mat4::Mat4;
 
 #[derive(Debug)]
-pub struct Quat(pub [f32;4]);
+pub struct Quat(pub [f32; 4]);
 
 //Internal functions which makes less sense
-pub fn extract_row(a : &Mat4, index : usize) -> Vec4
-{
-  Vec4(
-    [ 
-      a[0][index],
-      a[1][index],
-      a[2][index],
-      a[3][index]
+pub fn extract_row(a: &Mat4, index: usize) -> Vec4 {
+    Vec4([a[0][index], a[1][index], a[2][index], a[3][index]])
+}
+
+pub fn quat_new() -> Quat {
+    Quat([0.0, 0.0, 0.0, 1.0])
+}
+//Assume lower row is 0_0_0_1
+pub fn mat4_mul_vec3(a: &Mat4, b: &Vec3) -> Vec3 {
+    let row0 = extract_row(&a, 0);
+    let row1 = extract_row(&a, 1);
+    let row2 = extract_row(&a, 2);
+    //TODO: Don't create a new object here:
+    Vec3([
+        b.dot(Vec3([row0[0], row0[1], row0[2]])) + row0[3],
+        b.dot(Vec3([row2[0], row2[1], row2[2]])) + row2[3],
+        b.dot(Vec3([row1[0], row1[1], row1[2]])) + row1[3],
     ])
 }
 
-pub fn quat_new() -> Quat
-{
-  Quat([0.0, 0.0, 0.0, 1.0])
-}
-//Assume lower row is 0_0_0_1
-pub fn mat4_mul_vec3(a : &Mat4, b : &Vec3) -> Vec3
-{
-  let row0 = extract_row(&a, 0);
-  let row1 = extract_row(&a, 1);
-  let row2 = extract_row(&a, 2);
-  //TODO: Don't create a new object here:
-  Vec3([
-    b.dot(Vec3([row0[0], row0[1], row0[2]])) + row0[3],
-    b.dot(Vec3([row2[0], row2[1], row2[2]])) + row2[3],
-    b.dot(Vec3([row1[0], row1[1], row1[2]])) + row1[3],
-  ])
-}
-
-pub fn mat4_mul_vec4(a : &Mat4, b : &Vec4) -> Vec4
-{
-  let row0 = extract_row(&a, 0);
-  let row1 = extract_row(&a, 1);
-  let row2 = extract_row(&a, 2);
-  let row3 = extract_row(&a, 3);
-  Vec4([
-    Vec4::dot(&row0, &b),
-    Vec4::dot(&row1, &b),
-    Vec4::dot(&row2, &b),
-    Vec4::dot(&row3, &b),
-  ])
+pub fn mat4_mul_vec4(a: &Mat4, b: &Vec4) -> Vec4 {
+    let row0 = extract_row(&a, 0);
+    let row1 = extract_row(&a, 1);
+    let row2 = extract_row(&a, 2);
+    let row3 = extract_row(&a, 3);
+    Vec4([
+        Vec4::dot(&row0, &b),
+        Vec4::dot(&row1, &b),
+        Vec4::dot(&row2, &b),
+        Vec4::dot(&row3, &b),
+    ])
 }
