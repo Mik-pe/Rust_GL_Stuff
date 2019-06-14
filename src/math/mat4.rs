6c4fab15a2084e5d359e0196d6b6816072e39b86
Magrit <mikael.pettersson201@gmail.com>
2019-06-14 20:36:17 +0200

    Accidentally introduced a transposed ortho matrix

--- src/math/mat4.rs
+++ src/math/mat4.rs
@@ -144,15 +144,20 @@ impl Mat4 {
 
     pub fn create_ortho(bottom: f32, top: f32, left: f32, right: f32, near: f32, far: f32) -> Mat4 {
         Mat4([
-            Vec4([2.0 / (right - left), 0.0, 0.0, 0.0]),
-            Vec4([0.0, 2.0 / (top - bottom), 0.0, 0.0]),
-            Vec4([0.0, 0.0, -2.0 / (far - near), 0.0]), // <-- Revise negativity
             Vec4([
+                2.0 / (right - left),
+                0.0,
+                0.0,
                 -(right + left) / (right - left),
-                -(top + bottom) / (top - bottom),
-                -(far + near) / (far - near),
-                1.0,
             ]),
+            Vec4([
+                0.0,
+                2.0 / (top - bottom),
+                0.0,
+                -(top + bottom) / (top - bottom),
+            ]),
+            Vec4([0.0, 0.0, -2.0 / (far - near), -(far + near) / (far - near)]), // <-- Revise negativity
+            Vec4([0.0, 0.0, 0.0, 1.0]),
         ])
     }
 }
